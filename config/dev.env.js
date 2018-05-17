182c51c9e6a9f629c1c3d99a3aef275f7c23159c
caoanlong <476977540@qq.com>
2018-05-17 23:36:53 +0800

    ff

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
 	NODE_ENV: '"development"',
 	ENV_CONFIG: '"dev"',
-	// BASE_API: '"http://47.106.171.37:4444/api"',  // 线上
-	BASE_API: '"http://localhost:4444/api"',  // 测试
+	BASE_API: '"http://47.106.171.37:4444/api"',  // 线上
+	// BASE_API: '"http://localhost:4444/api"',  // 测试
 	IMG_API: '"http://47.106.171.37:3030"',
 })
